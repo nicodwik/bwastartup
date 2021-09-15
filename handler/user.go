9b94663eaa3a7ccb6b21b08d14a232802958b55b
nicodwik <nicodwika@gmail.com>
2021-09-15 13:57:39 +0700

    init commit + user

---
+++ handler/user.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct registerUserInput
+	// struct di atas kita passing sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Register failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if true {
+		response := helper.APIResponse("Register failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// token, err := h.jwtService.GenerateToken()
+
+	formatter := user.FormatUser(newUser, "asdasdascjaskasdasd")
+	response := helper.APIResponse("Account has been created", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+}
---
+++ user/formatter.go
@@ -0,0 +1,19 @@
+package user
+
+type UserFormatter struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Occupation string `json:"occupation"`
+	Token      string `json:"token"`
+}
+
+func FormatUser(user User, token string) UserFormatter {
+	formatter := UserFormatter{
+		ID:         user.Id,
+		Name:       user.Name,
+		Occupation: user.Occupation,
+		Token:      token,
+	}
+
+	return formatter
+}
